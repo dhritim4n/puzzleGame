c3d2f4207419d2be80ba10a7ceab03028bd2378e
Lenovo <Lenovo@WIN11>
2025-06-04 13:36:44 +0530

    .

--- src/Board.jsx
+++ src/Board.jsx
@@ -8,7 +8,10 @@ import { scrollBehaviourDragImageTranslateOverride } from 'mobile-drag-drop/scro
 
 
 
-
+polyfill({
+  dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride;
+  holdToDrag: 200
+});
 
 
 
